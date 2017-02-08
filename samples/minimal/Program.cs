4552f3bf3f90c7911e8b5964e660612a74771d01
Łukasz Klimek <klinn91@gmail.com>
2017-02-09 00:04:47 +0100

    added a minimal c# project

---
+++ samples/minimal/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Minimal
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Minimal hello world!");
+        }
+    }
+}
